7b9f383b51f5ed92336dbebe884010874c8db496
clg <30654236+clg04@users.noreply.github.com>
2018-07-17 14:11:54 +0800

    修改insert方法

--- tests/sqlTranslator.test.js
+++ tests/sqlTranslator.test.js
@@ -1,18 +1,30 @@
-'use strict'
+"use strict";
 
 const translator = require("../lib/sqlTranslator");
 const assert = require("assert");
-describe('sqlTranslatorTest.js', () => {
-    it('select in Test', () => {
-        const sql = `select 
+describe("sqlTranslatorTest.js", () => {
+  it("select in Test", () => {
+    const sql = `select 
     * from table
     where id=@Id and array in (@array) and name=@name
-    `
-        const a = translator.execute(sql, {
-            id: 1,
-            array: [1, 2, 3, 4],
-            name: '你好'
-        });
-        console.log('result',a);
-    })
-})
+    `;
+    const a = translator.execute(sql, {
+      id: 1,
+      array: [1, 2, 3, 4],
+      name: "你好"
+    });
+    console.log("result", a);
+  });
+  it("insert into Test", () => {
+    const sql = `insert into table (a,b,c,d,CreatorId,CreatorName)
+        values(@a,@b,@c,@d,@CreatorId,@CreatorName)
+    `;
+    const a = translator.insert(
+      sql,
+      [{ l: 5, d: 4, a: 1, c: 3, b: 2 }, { a: 1, l: 5, d: 4, b: 2, c: 3 }],
+      {aliasList:{k: "l"},
+      extraDatalist:{CreatorId: 2, CreatorName: "clg"} }
+    );
+    console.log("insert", a);
+  });
+});
