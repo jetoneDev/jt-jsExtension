55cdf38ca0127b9d39416c44be90c5658b6e0e98
tao_weng <jaikensai888@qq.com>
2018-07-17 14:34:24 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jt-jsExtension
+
+## log
+
+|1.0.1|Insert 支持无顺序，对照表，定制数据|
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const sqlTranslator = require('./lib/sqlTranslator');
 const arrayExtension = require('./lib/arrayExtension');
-
+const dateExtension = require('./lib/dateExtension');
 module.exports = {
     sqlTranslator,
-    arrayExtension
+    arrayExtension,
+    dateExtension,
 };
